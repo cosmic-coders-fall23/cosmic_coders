8bc520a621a7408f228b6e06fd3adf60572d35df
jramirez <justin.ramirez@gametime.co>
2023-11-01 00:58:37 -0400

    change auth/login to nextauth
    tmp
    
    tmp

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,59 +1,61 @@
 'use client';
-import { useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { Card, Input, Button } from "@nextui-org/react";
+import { signIn, getProviders, getCsrfToken } from 'next-auth/react';
 
-function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+const LoginPage: FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [csrfToken, setCsrfToken] = useState<string | null>(null);
+  const [providers, setProviders] = useState<Record<string, any>>({});
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch("/api/login", {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
-      });
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = await getCsrfToken();
+      const providerList = await getProviders();
+      setCsrfToken(token);
+      setProviders(providerList);
+    };
 
-      const result = await response.json();
-
-      if (response.ok) {
-        // Handle successful login, e.g., redirecting the user
-        alert(result.message);
-      } else {
-        setErrorMessage(result.message);
-      }
-    } catch (error) {
-      console.error("Login error:", error);
-      setErrorMessage("Error during login. Please try again.");
-    }
-  };
+    fetchData();
+  }, []);
 
   return (
     <div className="h-screen flex items-center justify-center p-5">
       <Card className="w-96 p-8">
         <h1 className="text-4xl text-center font-extrabold mb-4">Log In</h1>
-        {errorMessage && <span className="text-red-500 text-sm block">{errorMessage}</span>}
-        <div className="flex w-full flex-wrap gap-4">
+        {errorMessage && <span className="text-red-500 text-sm block mb-4">{errorMessage}</span>}
+        {providers && Object.values(providers).map(provider => (
+          <Button key={provider.name} className="w-full mb-4" onClick={() => signIn(provider.id)}>
+            Log in with {provider.name}
+          </Button>
+        ))}
+        <form method="post" action="/api/auth/callback/credentials">
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken || ''} />
           <Input 
-            type="email" 
-            label="Email" 
+            type="email"
+            label="Email"
             placeholder="Enter your email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            name="email"
           />
           <Input 
-            type="password" 
-            label="Password" 
+            type="password"
+            label="Password"
             placeholder="Enter your password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            name="password"
           />
-          <Button className="w-full bg-blue-700" onClick={handleLogin}>Log In</Button>
-        </div>
+          <Button className="w-full bg-blue-700 mt-4" type="submit">Log In</Button>
+        </form>
       </Card>
     </div>
   );
-}
+};
 
 export default LoginPage;
+
+
