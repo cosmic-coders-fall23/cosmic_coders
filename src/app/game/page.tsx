a91c03e262778ed303b629121cb37afb2aefe4f1
Justin Ramirez <ramirez.justin@gmail.com>
2023-11-02 03:19:29 -0400

    some game play

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,61 +1,123 @@
 "use client"
-import kaboom from "kaboom"
-import * as React from "react"
+import kaboom from "kaboom";
+import * as React from "react";
 
 const Game: React.FC = () => {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
-    const canvasRef = React.useRef(null)
+  React.useEffect(() => {
+    const k = kaboom({
+      global: true,
+      canvas: canvasRef.current as any,
+      background: [0, 0, 20],
+      width: 800,
+      height: 600,
+      scale: 1,
+      debug: true,
+    });
 
-    React.useEffect(() => {
-        const k = kaboom({
-            global: true,
-            canvas: canvasRef.current as any,
-            background: [0, 0, 20],
-            width: 800,
-            height: 600,
-            scale: 1,
-            debug: true,
-        })
+    // Load the sprites
+    k.loadSprite("spaceship", "sprites/starship.png");
+    k.loadSprite("alien", "sprites/alien.png"); // Assuming this is already available in your sprites folder
+    k.loadSprite("bullet", "sprites/laserBullet.png");
 
-        k.loadSprite("spaceship", "sprites/starship.png")
+    // Define the player
+    const player = k.add([
+      k.sprite("spaceship"),
+      k.pos(350, 500),
+      k.scale(0.5),
+      k.area(),
+      k.body(),
+    ]);
 
-        const player = k.add([
-            k.sprite("spaceship"),
-            k.pos(120, 80),
-            k.scale(1),
-            k.area(), 
-            k.body(),
-        ])
+    const moveScale = 100;
+    const bulletSpeed = 400;
 
-        const moveScale = 100
+    // Player controls
+    k.onKeyDown("left", () => {
+      player.move(k.vec2(-1, 0).scale(moveScale));
+    });
+    k.onKeyDown("right", () => {
+      player.move(k.vec2(1, 0).scale(moveScale));
+    });
+    k.onKeyDown("up", () => {
+      player.move(k.vec2(0, -1).scale(moveScale));
+    });
+    k.onKeyDown("down", () => {
+      player.move(k.vec2(0, 1).scale(moveScale));
+    });
 
-        k.onKeyDown("left", () => {
-            player.move(k.vec2(-1, 0).scale(moveScale))
-        })
-        k.onKeyDown("right", () => {
-            player.move(k.vec2(1, 0).scale(moveScale))
-        })
-        k.onKeyDown("up", () => {
-            player.move(k.vec2(0, -1).scale(moveScale))
-        })
-        k.onKeyDown("down", () => {
-            player.move(k.vec2(0, 1).scale(moveScale))
-        })
+    // Function to spawn bullets
+    function shoot() {
+      k.add([
+        k.sprite("bullet"),
+        k.pos(player.pos.add(-55, -155)),
+        k.area(),
+        k.move(k.UP, bulletSpeed),
+        "bullet",
+      ]);
+    }
 
-    }, [])
+    // Shooting with spacebar
+    k.onKeyPress("space", () => {
+      shoot();
+    });
 
-    return (
-        <div className="h-screen flex items-center justify-center p-5">
-            <div>
-                <div className="flex w-full flex-wrap gap-4">
-                    <div>
-                        {/* <h1 className="text-4xl text-center font-extrabold mb-4">GAME</h1> */}
-                        <canvas ref={canvasRef}></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+    // Define enemy behavior
+    function spawnEnemy() {
+      k.add([
+        k.sprite("alien"),
+        k.scale(0.3),
+        k.pos(k.rand(0, k.width()), -30),
+        k.area(),
+        k.body(),
+        k.move(k.DOWN, 120),
+        "enemy",
+      ]);
+    }
+
+    // Check collision of bullet with enemy
+    k.onCollide("bullet", "enemy", (bullet: any, enemy: any) => {
+      k.destroy(bullet);
+      k.destroy(enemy);
+    });
+
+    // Spawn an enemy every 2 seconds
+    k.loop(2, () => {
+      spawnEnemy();
+    });
+
+    // Define an action for all bullets to check for out of bounds
+    k.onCollide("bullet", "enemy", (bullet: any) => {
+      if (bullet.pos.y < 0) {
+        k.destroy(bullet);
+      }
+    });
+
+    // Check for collisions between the player and enemies
+    k.onCollide("enemy", "spaceship", (enemy: any) => { 
+      // End the game or reduce player's life
+      k.destroy(enemy);
+      // You can put game over logic or life deduction logic here
+    });
+
+    // Remove enemies when they go out of the game bounds
+    // k.action("enemy", (enemy: any) => {
+    //   if (enemy.pos.y > k.height()) {
+    //     k.destroy(enemy);
+    //   }
+    // });
+
+  }, []);
+
+  return (
+    <div className="h-screen flex items-center justify-center p-5">
+      <div>
+        <canvas ref={canvasRef}></canvas>
+      </div>
+    </div>
+  );
+};
 
 export default Game;
+
