61eea95107d4e2025e28f447ba47f6b840552e89
jramirez <justin.ramirez@gametime.co>
2023-11-02 21:26:40 -0400

    add score/ disable db

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "../../../../lib/mongodb";
 
 const handler = NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
+  // adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? "",
@@ -17,20 +17,14 @@ const handler = NextAuth({
           access_type: "offline",
           response_type: "code"
         }
-      } 
+      }
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
-  // pages: {
-  //   signIn: '/api/auth/signin',
-  //   signOut: '/api/auth/signout',
-  //   // error: '/auth/error', // Error code passed in query string as ?error=
-  //   verifyRequest: '/auth/verify-request', // (used for check email message)
-  //   newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-  // }
+
 });
 
 export { handler as GET, handler as POST };
