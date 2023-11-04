2a4ee163bdb15bae72692a49833359606bc8a8b9
sbhat8 <sbhatawadekar1@student.gsu.edu>
2023-11-04 17:49:18 -0400

    Add Dockerfile and home page now renders image with nextui image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,15 @@
 //ap/page.tsx
 'use client'
-import React, { useEffect } from 'react';
+import React from 'react';
+import {Image} from "@nextui-org/react";
 
-const Home: React.FC = () => {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
-  
-  React.useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas?.getContext('2d');
-
-    // Load and draw the logo image on the canvas
-    const logo = new Image();
-    logo.src = "/cosmiccoderslogo.png";
-    logo.onload = () => {
-      if (context && canvas) {
-        context.drawImage(logo, (canvas.width - logo.width)/2, (canvas.height - logo.height)/2);
-      }
-    }
-  }, []);
+function HomePage() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
-      <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} />
+      <Image className="z-20 h-full" src="/cosmiccoderslogo.png"/>
     </div>
   );
 }
 
-export default Home;
+export default HomePage;
